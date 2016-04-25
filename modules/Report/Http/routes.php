4afe610ddbc6200dbc659c991d0d9448c397fc6c
MichalGallovic <michalgallovic@MacBook-Pro.local>
2016-04-25 19:54:29 +0200

    Add reports CRUD

--- modules/Report/Http/routes.php
+++ modules/Report/Http/routes.php
@@ -3,6 +3,9 @@
 Route::group(['prefix' => 'report', 'namespace' => 'Modules\Report\Http\Controllers'], function()
 {
 	Route::get('/', ['as' => 'report.index', 'uses' => 'ReportController@index']);
+	Route::get('{id}', ['as' => 'report.show', 'uses' => 'ReportController@show']);
+	Route::post('{id}', ['as' => 'report.update', 'uses' => 'ReportController@update']);
+	Route::delete('{id}', ['as' => 'report.delete', 'uses' => 'ReportController@delete']);
 });
 
 Route::group(["prefix" => "api", "namespace" => 'Modules\Report\Http\Controllers'], function() {
