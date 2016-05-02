1e9a1de8f162f3f07485e3ffd487c175228c975a
MichalGallovic <gallovicm@gmail.com>
2016-05-02 23:18:52 +0200

    Cleaned code for experiment running

--- modules/Experiments/Jobs/RunExperimentJob.php
+++ modules/Experiments/Jobs/RunExperimentJob.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Jobs\Job;
 use Illuminate\Support\Arr;
 use App\Services\ReportService;
+use App\Services\ExperimentRunner;
 use App\Services\ExperimentValidator;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
@@ -14,6 +15,7 @@ use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Modules\Experiments\Entities\Experiment;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use App\Exceptions\Experiments\DeviceNotReady;
 use Modules\Experiments\Jobs\RunExperimentJob;
 use Modules\Experiments\Entities\PhysicalDevice;
 use Modules\Experiments\Entities\ServerExperiment;
@@ -49,39 +51,15 @@ class RunExperimentJob extends Job implements SelfHandling, ShouldQueue
      */
     public function handle()
     {
-        var_dump($this->experiment->device->name . " " . $this->experiment->software->name . " queued!");
-        $instanceName = Arr::get($this->input,"instance");
 
-        if($instanceName) {
-            $physicalDevice = PhysicalDevice::ready()->ofDevice($this->input['device'])->ofName($this->input['instance'])->first();
-        } else {
-            $physicalDevice = PhysicalDevice::ready()->ofDevice($this->input['device'])->first();
-        }
-
-        if($physicalDevice) {
-            $physicalExperiment = PhysicalExperiment::where('experiment_id', $this->experiment->id)->where('physical_device_id', $physicalDevice->id)->first();
-
-            $validator = new ExperimentValidator($physicalExperiment->rules->toArray(), $this->input['input']);
-
-            if($validator->fails()) {
-                return false;
-                // throw new ValidationException($validator->errors());
-            }
-
-            $report = new ReportService();
-            $reportId = $report->create($this->user, $physicalExperiment, $this->input);
-            $this->input = array_merge($this->input, [
-                    "report_id" => $reportId
-                ]);
-
-
-            $server = new Server($physicalDevice->server->ip);
-            $server->queueExperiment($this->input);
-            if($server->success()) {
-                $physicalDevice->status = "experimenting";
-                $physicalDevice->save();
-            }
-        } else {
+        try {
+            $runner = new ExperimentRunner(
+                $this->user, $this->experiment, $this->input
+            );
+            $runner->run();
+        } catch(DeviceNotReady $e) {
+            // The experiment should be postponed
+            // $e->nextTrySeconds();
             $job = (new RunExperimentJob($this->user, $this->experiment, $this->input))->delay(5);
             $this->dispatch($job);
         }
